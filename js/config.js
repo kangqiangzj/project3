d536602ecdb69cd5b493d2be8868b7c1008c0db3
zhangjie <1148291351@qq.com>
2017-11-29 09:16:53 +0800

    更新了jquery

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 require.config({
 	baseUrl : "/",
 	paths : {
-		"jquery" : "httpshop213.v.ev123.netvip_shop213.html/lib/jquery/jquery-1.12.4.min",//["https://code.jquery.com/jquery-1.12.4.min.js","lib/jquery/jquery-1.1.12.4"]
+		"jquery" : "httpshop213.v.ev123.netvip_shop213.html/lib/jquery/jquery-1.9.1.min",//["https://code.jquery.com/jquery-1.12.4.min.js","lib/jquery/jquery-1.1.12.4"]
 		"cookie" : "httpshop213.v.ev123.netvip_shop213.html/lib/jquery_plugins/jquery.cookie",
 		"load" : "httpshop213.v.ev123.netvip_shop213.html/js/loadHeaderFooter",
 		"zoom" : "httpshop213.v.ev123.netvip_shop213.html/lib/jquery_plugins/jquery.elevateZoom-3.0.8.min"
